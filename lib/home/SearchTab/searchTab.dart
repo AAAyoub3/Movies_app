7d5ca7eaf634dd1a0462d5df5aa3815e54e49940
Administrator <abdelrahmanobo12@gmail.com>
2023-11-08 00:29:27 +0200

    font size is 14

--- lib/home/SearchTab/searchTab.dart
+++ lib/home/SearchTab/searchTab.dart
@@ -18,7 +18,7 @@ class SearchTab extends StatelessWidget {
           ),
           child: TextField(
             style: TextStyle(
-              fontSize: 15,
+              fontSize: 14,
               color: Colors.black,
             ),
             decoration: InputDecoration(
