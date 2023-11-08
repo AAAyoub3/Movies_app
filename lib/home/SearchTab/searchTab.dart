cc1c4ff6affe108e16ee8ea13956ef35942ef6d9
Administrator <abdelrahmanobo12@gmail.com>
2023-11-08 22:31:46 +0200

    Update SingleChildViewScroll

--- lib/home/SearchTab/searchTab.dart
+++ lib/home/SearchTab/searchTab.dart
@@ -5,49 +5,51 @@ class SearchTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        SizedBox(height: MediaQuery.of(context).size.height*0.08,),
-        /// search
-        Container(
-          margin: const EdgeInsets.all(10),
-          height: MediaQuery.of(context).size.height*0.07,
-          child: TextField(
-            style: Theme.of(context).textTheme.bodyMedium,
-            decoration: InputDecoration(
-              prefixIcon: Icon(
-                Icons.search_rounded,
-                color: MyThemeData.whiteColor,
-              ),
-              hintText: "Search",
-              hintStyle: Theme.of(context).textTheme.bodyMedium,
-              fillColor: MyThemeData.greyColor,
-              filled: true,
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-                borderSide: BorderSide(
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          SizedBox(height: MediaQuery.of(context).size.height*0.08,),
+          /// search
+          Container(
+            margin: const EdgeInsets.all(10),
+            height: MediaQuery.of(context).size.height*0.07,
+            child: TextField(
+              style: Theme.of(context).textTheme.bodyMedium,
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.search_rounded,
                   color: MyThemeData.whiteColor,
-                  width: 1
                 ),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-                borderSide: BorderSide(
-                  color: MyThemeData.whiteColor,
-                  width: 1
+                hintText: "Search",
+                hintStyle: Theme.of(context).textTheme.bodyMedium,
+                fillColor: MyThemeData.greyColor,
+                filled: true,
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide(
+                    color: MyThemeData.whiteColor,
+                    width: 1
+                  ),
                 ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide(
+                    color: MyThemeData.whiteColor,
+                    width: 1
+                  ),
+                ),
+                //icon: Icon(Icons.search_rounded)
               ),
-              //icon: Icon(Icons.search_rounded)
             ),
           ),
-        ),
 
-        /// no movies found
-        Center(
-          heightFactor: MediaQuery.of(context).size.height*0.0062,
-            child: Image.asset("assets/images/Group 22.png")),
-      ],
+          /// no movies found
+          Center(
+            heightFactor: MediaQuery.of(context).size.height*0.0062,
+              child: Image.asset("assets/images/Group 22.png")),
+        ],
+      ),
     );
   }
 }
