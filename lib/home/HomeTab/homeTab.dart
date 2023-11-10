fb7ae603b65eee72bd7f4ea05e18e6277d65d14a
Administrator <abdelrahmanobo12@gmail.com>
2023-11-10 12:02:21 +0200

    Adjust width for popular width and adding swap timer for it

--- lib/home/HomeTab/homeTab.dart
+++ lib/home/HomeTab/homeTab.dart
@@ -3,8 +3,8 @@ import 'package:movies/API/api_manager.dart';
 import 'package:movies/home/HomeTab/HorizontalSliderWidget.dart';
 import 'package:movies/home/HomeTab/popular_movies_widget.dart';
 import 'package:movies/model/popularResource.dart';
-import 'package:movies/myTheme.dart';
-import 'StackButtonWidget.dart';
+import 'dart:async';
+
 
 class HomeTab extends StatefulWidget {
   @override
@@ -12,34 +12,61 @@ class HomeTab extends StatefulWidget {
 }
 
 class _HomeTabState extends State<HomeTab> {
+  late PageController _pageController;
+  late var list;
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+
+    Timer.periodic(const Duration(seconds: 10), (Timer timer) { /// Duration to the run this code section
+        var nextPage = _pageController.page! + 1;
+
+        if (nextPage >= list!.length) { /// Reached the end then start over
+          _pageController.jumpToPage(0);
+        }
+        else {
+          _pageController.nextPage( /// Switching to the next page
+            duration: const Duration(seconds: 1), /// swapping duration
+            curve: Curves.easeInOut, /// Speed of swapping
+            /// easeIn : slow start , easeOut : slow end , easeInOut smooth transition between start & end
+          );
+        }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<PopularResource?>(future: ApiManager.getPopular(),
-        builder: (context,snapshot){
-          if(snapshot.connectionState == ConnectionState.waiting){  /// If he is still loading
-            return Center(child: CircularProgressIndicator(color: Theme.of(context).primaryColor));
+        builder: (context, snapshot) {
+
+          /// If he is still loading
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator(color: Theme
+                .of(context)
+                .primaryColor));
           }
+
           /// User Error
-          else if (snapshot.hasError){
+          else if (snapshot.hasError) {
             return Column(
               children: [
                 Text(snapshot.data?.status_message ?? ''),
-                ElevatedButton(onPressed: (){}, child: const Text("Try Again"))
+                ElevatedButton(onPressed: () {}, child: const Text("Try Again"))
               ],
             );
           }
 
           /// API Error
-          if (snapshot.data?.results == null){
+          if (snapshot.data?.results == null) {
             return Column(
               children: [
                 Text(snapshot.data?.status_message ?? ''),
-                ElevatedButton(onPressed: (){}, child: const Text("Try Again"))
+                ElevatedButton(onPressed: () {}, child: const Text("Try Again"))
               ],
             );
           }
-
-          var list = snapshot.data?.results;
+          list = snapshot.data?.results; /// list for popular
 
           return Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -47,13 +74,22 @@ class _HomeTabState extends State<HomeTab> {
 
               /// popular
               SizedBox(
-                height: MediaQuery.of(context).size.height*0.42,
+                height: MediaQuery
+                    .of(context)
+                    .size
+                    .height * 0.37,
                 width: double.infinity,
-                child: ListView.builder(
+                child: PageView.builder(
+                  controller: _pageController,
                   scrollDirection: Axis.horizontal,
-                  itemBuilder:(context, index) => PopularMoviesWidget(index: index, list: list) ,
+                  itemBuilder: (context, index) =>
+                      PopularMoviesWidget(
+                        index: index,
+                        list: list,
+                      ),
                   itemCount: list!.length,
-                ),),
+                ),
+              ),
 
               /// new releases
               HorizontalSliderWidget(title: "New Releases",
@@ -67,9 +103,7 @@ class _HomeTabState extends State<HomeTab> {
 
             ],
           );
-
         });
-
-
   }
 }
+
--- lib/home/HomeTab/popular_movies_widget.dart
+++ lib/home/HomeTab/popular_movies_widget.dart
@@ -9,62 +9,72 @@ class PopularMoviesWidget extends StatelessWidget{
   PopularMoviesWidget({required this.index,required this.list});
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.bottomLeft,
-      children: [
-        /// video
-        Column(
-          children: [
-            Stack(
-                alignment: Alignment.center,
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Stack(
+        alignment: Alignment.bottomLeft,
+        children: [
+          /// video
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: [
+                Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Image.network("https://image.tmdb.org/t/p/w500${list![index].backdropPath}"),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.play_circle),
+                        color: MyThemeData.whiteColor,
+                        iconSize: 100,
+                      ),
+                    ]
+                ),
+              ],
+            ),
+          ),
+
+          /// movie's pic && data
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  Image.network("https://image.tmdb.org/t/p/w500${list![index].backdropPath}"),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.play_circle),
-                    color: MyThemeData.whiteColor,
-                    iconSize: 100,
+                  /// pic
+                  StackButtonWidget(
+                    imgPath: "https://image.tmdb.org/t/p/w500${list![index].posterPath}",
                   ),
+
+                  /// data
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: Text(list![index].title,
+                              style: Theme.of(context).textTheme.titleSmall,
+                              maxLines: 3,
+                              overflow: TextOverflow.ellipsis,
+                          ),
+
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(list![index].releaseDate,
+                              style: Theme.of(context).textTheme.titleSmall
+                                  ?.copyWith(fontSize: 10)),
+                        ),
+                      ],
+                    ),
+                  )
                 ]
             ),
-            SizedBox(height: MediaQuery.of(context).size.height*0.07),
-          ],
-        ),
-
-
-        /// movie's pic && data
-        Padding(
-          padding: const EdgeInsets.only(bottom: 11),
-          child: Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                /// pic
-                StackButtonWidget(
-                  imgPath: "https://image.tmdb.org/t/p/w500${list![index].posterPath}",
-                ),
-                /// data
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 8.0, left: 8.0),
-                      child: Text(list![index].title,
-                          style: Theme.of(context).textTheme.titleSmall),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(list![index].releaseDate,
-                          style: Theme.of(context).textTheme.titleSmall
-                              ?.copyWith(fontSize: 10)),
-                    ),
-                  ],
-                )
-              ]
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
-
 }
